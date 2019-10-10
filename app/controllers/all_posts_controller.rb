95187495244ccc7667d1f46b1b56163cc1caf008
Nagasawa0001 <50533812+Nagasawa0001@users.noreply.github.com>
2019-10-10 22:26:35 +0900

    都道府県検索を実装
    都道府県のIDで検索できる機能を実装しました

--- app/controllers/all_posts_controller.rb
+++ app/controllers/all_posts_controller.rb
@@ -7,6 +7,19 @@ class AllPostsController < ApplicationController
         @posts = Post.joins(:prefecture, :category).select("posts.*, prefectures.prefecture, categories.category")
         @categories = Category.all
     end
+    
+    def search_prefecture
+        
+        @posts = Post.joins(:prefecture, :category).where(prefecture_id: params[:prefecture_id]).select("posts.*, prefectures.prefecture, categories.category")
+        @categories = Category.all
+        render 'all_post'
+    end
 
+
+    def search_category
+    end
+
+    def search_word
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   get 'new_post', to: 'new_posts#new_post'
   get 'post_detail', to: 'post_details#post_detail'
   post 'send_post', to: 'new_posts#send_post'
+
+  get 'search_cate', to: 'all_posts#search_category'
+  get 'search_word', to: 'all_posts#search_word'
+  get 'search_pref', to: 'all_posts#search_prefecture'
 end
