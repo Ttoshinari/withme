01aff9bd03bda746efa63c6de8bcb4221581122e
Nagasawa0001 <50533812+Nagasawa0001@users.noreply.github.com>
2019-11-03 19:53:52 +0900

    mypage-top画面の表示実装、削除編集は未実装

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,5 +1,9 @@
 class MypageController < ApplicationController
   def top
+    @users = User.where(id: params[:id]).select("users.*")
+    @posts = Post.where(user_id: params[:id]).select("posts.*")
+    @postNumber = Post.where(user_id: params[:id]).count
+
   end
 
   def edit
